ae1b04e368f4d74d7252c56dbda638496b9d19b4
luubarcelona <117042060+luubarcelona@users.noreply.github.com>
2026-01-17 18:28:11 +0100

    done

--- init.js
+++ init.js
@@ -1,26 +1,22 @@
-
 const canvas = document.getElementById("miCanvas")
 const ctx = canvas.getContext("2d")
-    const frogger = {
-        postionX:700,
-        postionY:500,
-        width:100,
-        heigth:100,
+const frogger = {
+    postionX:700,
+    postionY:500,
+    width:100,
+    height:100, // CORREGIDO
 }
 
 function drawFrogger() {
     ctx.fillStyle = "green"
-     ctx.fillRect(
+    ctx.fillRect(
         frogger.postionX,
         frogger.postionY, 
         frogger.width, 
-        frogger.heigth
+        frogger.height
     )
 }
 drawFrogger()
-
-
-
 
 document.addEventListener("keydown", function(event) {
   if(event.key === "ArrowUp") {
@@ -31,3 +27,15 @@ document.addEventListener("keydown", function(event) {
         drawFrogger()
   }
 })
+
+document.addEventListener("keydown", function(event) {
+  if(event.key === "ArrowDown") {
+       if(frogger.postionY + 200 <= 600){ // ajusté a 100 para coincidir con el movimiento
+        frogger.postionY += 100
+    }
+       ctx.clearRect(0, 0, canvas.width, canvas.height)
+        drawFrogger()
+  }
+})
+
+
