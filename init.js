b16448290728ef7437354b1485d81f61666a82ea
luubarcelona <117042060+luubarcelona@users.noreply.github.com>
2026-01-26 04:34:20 +0100

    done

--- init.js
+++ init.js
@@ -1,133 +1,93 @@
-
-
 const canvas = document.getElementById("miCanvas")
 const ctx = canvas.getContext("2d")
-
-const speeds = 2;
-
-// Coche 1
-const car = {
-positionX: 0,
-positionY: 400,
-width: 150,
-height: 70,
-color:"red"
-}
-
-// Coche 2
-const tractor = {
-positionX: car.positionX + 330,
-positionY: 400,
-width: 150,
-height: 70,
-color:"blue"
-}
-// Coche 3
-
-const bus = {
-positionX: tractor.positionX + 330,
-positionY: 400,
-width: 150,
-height: 70,
-color:"magenta"
-}
-
-// Coche 4
-const scrooter = {
-positionX: bus.positionX + 330,
-positionY: 400,
-width: 150,
-height: 70,
-color:"red"
-}
-
-// Coche 5
-const truck = {
-positionX: scrooter.positionX + 330,
-positionY: 400,
-width: 150,
-height: 70,
-color:"purple"
-}
-
-
-
-// Rana
 const frogger = {
     positionX:700,
     positionY:500,
     width:80,
-    height:80
+    height:80,
 }
 
 function drawFrogger() {
-    ctx.fillStyle = "green";
-    ctx.fillRect(frogger.positionX, frogger.positionY, frogger.width, frogger.height);
+    ctx.fillStyle = "green"
+    ctx.fillRect(frogger.positionX, frogger.positionY, frogger.width, frogger.height)
 }
+
+const speeds = 2
+const cars = []
+const logs = []
+
+const logsTemplate = {
+    positionX: 1630,
+    positionY: 200,
+    width: 150,
+    height: 70,
+    color: "red",
+
+}
+
+const carTemplate = {
+    positionX: -150,
+    positionY: 400,
+    width: 150,
+    height: 70,
+    color: "blue", 
+}
+
+setInterval(() => {
+    cars.push({ ...carTemplate})
+}, 2500);
+
+setInterval(() => {
+    logs.push({ ...logsTemplate })
+}, 2500); 
 
 function moveCars() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
 
-    // Coche 1
-    car.positionX += speeds;
-    if (car.positionX > canvas.width) {
-        car.positionX = -car.width;
+    for (let i = 0; i < cars.length; i++) {
+        cars[i].positionX += speeds
+
+        if (cars[i].positionX  > canvas.width) {
+            cars.splice(i, 1)
+            i--
+            continue
+        }
+
+        ctx.fillStyle = cars[i].color
+        ctx.fillRect(cars[i].positionX, cars[i].positionY, cars[i].width, cars[i].height)
     }
-    ctx.fillStyle = car.color;
-    ctx.fillRect(car.positionX, car.positionY, car.width, car.height);
 
-    // Coche 2
-    tractor.positionX += speeds;
-    if (tractor.positionX > canvas.width) {
-        tractor.positionX = -tractor.width;
+    for (let i = 0; i < logs.length; i++) {
+        logs[i].positionX -= speeds
+
+        if (logs[i].positionX + logs[i].width < 0) {
+            logs.splice(i, 1)
+            i--
+            continue
+        }
+        ctx.fillStyle = logs[i].color
+        ctx.fillRect(logs[i].positionX, logs[i].positionY, logs[i].width, logs[i].height)
     }
-    ctx.fillStyle = tractor.color;
-    ctx.fillRect(tractor.positionX, tractor.positionY, tractor.width, tractor.height);
-
-    // Coche 3
-    bus.positionX += speeds;
-    if (bus.positionX > canvas.width) {
-        bus.positionX = -bus.width;
-    }
-    ctx.fillStyle = bus.color;
-    ctx.fillRect(bus.positionX, bus.positionY, bus.width, bus.height);
-
-    // Coche 4
-    scrooter.positionX += speeds;
-    if (scrooter.positionX > canvas.width) {
-        scrooter.positionX = -scrooter.width;
-    }
-    ctx.fillStyle = scrooter.color;
-    ctx.fillRect(scrooter.positionX, scrooter.positionY, scrooter.width, scrooter.height);
-
-    // Coche 5
-    truck.positionX += speeds;
-    if (truck.positionX > canvas.width) {
-        truck.positionX = -truck.width;
-    }
-    ctx.fillStyle = truck.color;
-    ctx.fillRect(truck.positionX, truck.positionY, truck.width, truck.height);
-
-    drawFrogger();
-    requestAnimationFrame(moveCars);
+    drawFrogger()
+    requestAnimationFrame(moveCars)
 }
+moveCars()
 
-moveCars();
 document.addEventListener("keydown", function(event) {
   if(event.key === "ArrowUp" && frogger.positionY - 100 >= 0) {
-      frogger.positionY -= 100;
+      frogger.positionY -= 100
   }
 
   if(event.key === "ArrowDown" && frogger.positionY + frogger.height + 100 <= canvas.height) {
-      frogger.positionY += 100;
+      frogger.positionY += 100
   }
 
   if(event.key === "ArrowRight" && frogger.positionX + frogger.width + 100 <= canvas.width) {
-      frogger.positionX += 100;
+      frogger.positionX += 100
   }
 
   if(event.key === "ArrowLeft" && frogger.positionX - 100 >= 0) {
-      frogger.positionX -= 100;
+      frogger.positionX -= 100
   }
 
 })
