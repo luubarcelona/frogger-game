0739d64b9c327c7ec7216f64ca6f7e540ef55a5a
luubarcelona <117042060+luubarcelona@users.noreply.github.com>
2026-02-04 15:12:46 +0100

    add collition

--- init.js
+++ init.js
@@ -2,14 +2,13 @@ const canvas = document.getElementById("miCanvas")
 const ctx = canvas.getContext("2d")
 const frogger = {
     positionX:700,
-    positionY:500,
-    width:80,
-    height:80,
-    death:false,
+    positionY:540,
+    width:50,
+    height:50,
 }
 
 function drawFrogger() {
-    ctx.fillStyle = "green"
+    ctx.fillStyle = "red"
     ctx.fillRect(frogger.positionX, frogger.positionY, frogger.width, frogger.height)
 }
 
@@ -19,11 +18,26 @@ const cars = []
 
 const carTemplate = {
     positionX: -150,
-    positionY: 400,
-    width: 150,
-    height: 70,
-    color: "green", 
+    positionY: 470,
+    width: 120,
+    height: 60,
+    color: "blue", 
 }
+
+function colliding(frogger, cars) {
+    const collition = cars.some(car => 
+        frogger.positionX <= car.positionX + car.width &&  
+        frogger.positionX + frogger.width >= car.positionX &&
+        frogger.positionY <= car.positionY + car.height && 
+        frogger.positionY + frogger.height >= car.positionY
+    )
+    return collition
+}
+
+
+
+
+
 
 setInterval(() => {
     cars.push({ ...carTemplate})
@@ -41,6 +55,7 @@ function moveCars() {
     drawFrogger()
     if (colliding(frogger, cars)) {
     console.log("¡Rana atropellada!");
+
     // aquí podrías reiniciar la rana o detener el juego
 }
     requestAnimationFrame(moveCars)
@@ -48,41 +63,21 @@ function moveCars() {
 moveCars()
 
 
-
-
-
-function colliding(frogger, cars) {
-    const collition = cars.some(car => 
-        // retornamos true si hay choque
-        frogger.positionX <= car.positionX + car.width &&  
-        frogger.positionX + frogger.width >= car.positionX &&
-        frogger.positionY <= car.positionY + car.height && 
-        frogger.positionY + frogger.height >= car.positionY
-    );
-
-    if (collition) {
-        frogger.death = true;
-    }
-
-    return collition;
-}
-
-
 document.addEventListener("keydown", function(event) {
-  if(event.key === "ArrowUp" && frogger.positionY - 100 >= 0) {
-      frogger.positionY -= 100
+  if(event.key === "ArrowUp" && frogger.positionY - 60 >= 0) {
+      frogger.positionY -= 60
   }
 
-  if(event.key === "ArrowDown" && frogger.positionY + frogger.height + 100 <= canvas.height) {
-      frogger.positionY += 100
+  if(event.key === "ArrowDown" && frogger.positionY + frogger.height + 60 <= canvas.height) {
+      frogger.positionY += 60
   }
 
-  if(event.key === "ArrowRight" && frogger.positionX + frogger.width + 100 <= canvas.width) {
-      frogger.positionX += 100
+  if(event.key === "ArrowRight" && frogger.positionX + frogger.width + 60 <= canvas.width) {
+      frogger.positionX += 60
   }
 
-  if(event.key === "ArrowLeft" && frogger.positionX - 100 >= 0) {
-      frogger.positionX -= 100
+  if(event.key === "ArrowLeft" && frogger.positionX - 60 >= 0) {
+      frogger.positionX -= 60
   }
 
 
